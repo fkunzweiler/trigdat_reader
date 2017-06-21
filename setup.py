5ce451a5d4405f0faf7c3e25d231d2f18af59054
grburgess <jmichaelburgess@gmail.com>
2017-06-21 10:28:00 +0200

    adding setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='trigdat_reader',
+    version='0.1',
+    description='Reads GBM trigdat data and creates plugins',
+    author='J. Michael Burgess',
+    author_email='jmichaelburgess@gmail.com',
+    packages=['trigdat_reader'])
